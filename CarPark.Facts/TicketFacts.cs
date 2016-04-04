ce93336eeb1c7105566f77cb8b5e59b394704ab6
Kitsakorn Attchoo <kitsakorn@gmail.com>
2016-04-04 14:13:59 +0700

    checkin all

---
+++ CarPark.Facts/TicketFacts.cs
@@ -0,0 +1,102 @@
+﻿using CarPark.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace CarPark.Facts
+{
+    public class TicketFacts
+    {
+
+        public class General
+        {
+
+            [Fact]
+            public void BasicUsage()
+            {
+                Ticket t = new Ticket();
+                t.PlateNo = "1707";
+                t.DateIn = new DateTime(2016, 1, 1, 9, 0, 0);
+                t.DateOut = DateTime.Parse("13:30");
+
+             
+
+                Assert.Equal("1707", t.PlateNo);
+                Assert.Equal(9, t.DateIn.Hour);
+                Assert.Equal(13, t.DateOut.Hour);
+            }   
+
+        }
+
+        public class ParkingFeeProperty
+        {
+            [Fact]
+            public void First15Minutes_Free() {
+
+                //arrange
+                var t = new Ticket();
+                t.PlateNo = "155";
+                t.DateIn = DateTime.Parse("9:00");
+                t.DateOut = DateTime.Parse("9:15");
+
+                // act
+                decimal fee = t.ParkingFee;
+
+                // assert
+                Assert.Equal(0m, fee);
+
+                
+            }
+
+            [Fact]
+            public void WithInFirst3Hours_50Baht()
+            {
+                //arrange
+                var t = new Ticket();
+                t.PlateNo = "155";
+                t.DateIn = DateTime.Parse("9:00");
+                t.DateOut = DateTime.Parse("9:15:01");
+
+                // act
+                decimal fee = t.ParkingFee;
+
+                // assert
+                Assert.Equal(50m, fee);
+            }
+            [Fact]
+            public void WithInFirst3HoursII_50Baht()
+            {
+                //arrange
+                var t = new Ticket();
+                t.PlateNo = "155";
+                t.DateIn = DateTime.Parse("9:00");
+                t.DateOut = DateTime.Parse("12:11:01");
+
+                // act
+                decimal fee = t.ParkingFee;
+
+                // assert
+                Assert.Equal(50m, fee);
+            }
+            [Fact]
+            public void WithInFirst4Hours_50Baht()
+            {
+                //arrange
+                var t = new Ticket();
+                t.PlateNo = "155";
+                t.DateIn = DateTime.Parse("9:00");
+                t.DateOut = DateTime.Parse("15:11");
+
+                // act
+                decimal fee = t.ParkingFee;
+
+                // assert
+                Assert.Equal(110m, fee);
+            }
+        }
+    }
+}
---
+++ CarPark.Models/Ticket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPark.Models
+{
+    public class Ticket
+
+    {
+      
+        public DateTime DateIn { get; set; }
+        public DateTime ? DateOut  { get; set; }
+        public decimal ParkingFee
+        {
+            get
+            {
+                if (DateOut == null)
+                    throw new Exception("Not DateOut Value")
+                TimeSpan ans = DateOut.Value - DateIn;
+
+                if (ans.TotalSeconds <= (15 * 60))
+                    return 0;
+                else if (ans.TotalMinutes <= (3 * 60) + 15)
+                    return 50;
+                else
+                {
+                    ans = ans - new TimeSpan(3, 0, 0);
+                    Int32 fee = 30;
+                    if (ans.Minutes > 15  )
+                        return (Convert.ToInt32(ans.TotalHours) * fee) + 50;
+                    else
+                        return ((Convert.ToInt32(ans.TotalHours) - 1) * fee) + 50;
+                }
+            }
+        }
+        public string PlateNo
+        {
+            get; set;
+
+
+        }
+    }
+}
